129146bed33c7fef0efb8fe40d29307317591837
KhojiAkbarraxim <xraximovx020304@gmail.com>
2024-03-12 12:58:42 +0500

    crud begin

---
+++ resources/views/CUD/cnews.blade.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Маҳалла фуқоралар йиғинлари</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- ======= Top Bar ======= -->
+    <div id="topbar" class="fixed-top d-flex align-items-center topbar-inner-pages">
+        <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
+            <div class="contact-info d-flex align-items-center d-none d-md-block">
+                <i class="bi bi-envelope-fill"></i><span>info@urganchhokimiyat.uz</span>
+                <i class="bi bi-phone-fill phone-icon"></i><span>+998 (62) 352-28-43</span>
+            </div>
+        </div>
+    </div>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-center header-inner-pages">
+        <div class="container d-flex align-items-center justify-content-start">
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Туман хокимлиги</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Туман ҳокимлиги аппарати</a></li>
+                            <li><a class="dropdown-item" href="#">Бошқарув аппаратининг тузилмаси</a></li>
+                            <li><a class="dropdown-item" href="#">Қабул жадвали</a></li>
+                            <li><a class="dropdown-item" href="#">Ишонч ва ягона телефон рақамлари</a></li>
+                            <li><a class="dropdown-item" href="#">Ҳорижий ва халқаро ҳамкорлик алоқалари</a></li>
+                            <li><a class="dropdown-item" href="#">Бошқа давлат органлари билан ўзаро муносабатлар</a></li>
+                            <li><a class="dropdown-item" href="#">Маҳаллий ва халқаро битимлар, шартномалар</a></li>
+                            <li><a class="dropdown-item" href="#">Кун тартиби, иш режаси ва йиғилиш масалалари</a></li>
+                            <li><a class="dropdown-item" href="#">Электрон ҳукумат доирасида қилинаётган ишлар</a></li>
+                            <li><a class="dropdown-item" href="#">Ҳокимлик иштирокидаги тадбирлар</a></li>
+                            <li><a class="dropdown-item" href="#">Бўш иш ўринлари</a></li>
+                            <li><a class="dropdown-item" href="#">Манзил</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                            Туман ҳақида
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Янгиликлар</a></li>
+                            <li><a class="dropdown-item" href="#">Тендер ва эълонлар</a></li>
+                            <li><a class="dropdown-item" href="#">Туман ҳақида маълумот</a></li>
+                            <li><a class="dropdown-item" href="#">Туман ташкилотлари</a></li>
+                            <li><a class="dropdown-item" href="#">Маҳалла фуқоралар йиғинлари</a></li>
+                            <li><a class="dropdown-item" href="#">Олий таълим муассасалари</a></li>
+                            <li><a class="dropdown-item" href="#">Мактабгача таълим муассасалари</a></li>
+                            <li><a class="dropdown-item" href="#">Умумий ўрта таълим мактаблар</a></li>
+                            <li><a class="dropdown-item" href="#">Касб-ҳунар коллежлари</a></li>
+                            <li><a class="dropdown-item" href="#">Статистикалар</a></li>
+                            <li><a class="dropdown-item" href="#">Экология</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Электрон хизматлар</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Мурожаатлар статистикаси</a></li>
+                            <li><a class="dropdown-item" href="#">Кўп бериладиган саволларга жавоблар</a></li>
+                            <li><a class="dropdown-item" href="#">Фуқаролар мурожаатлари</a></li>
+                            <li><a class="dropdown-item" href="#">Нотариал ҳаракатлар учун Давлат божи ставкалари</a></li>
+                            <li><a class="dropdown-item" href="#">Маҳаллий давлат ҳокимияти таркиби</a></li>
+                            <li><a class="dropdown-item" href="#">Ҳаж ва Умра зиёратига бориш тартиби</a></li>
+                            <li><a class="dropdown-item" href="#">Бизнесни режалаштириш ва бошқариш</a></li>
+                            <li><a class="dropdown-item" href="#">Тадбиркорликни лицензиялаш</a></li>
+                            <li><a class="dropdown-item" href="#">Алкоголли савдо фаолиятини лицензиялаш тартиби</a></li>
+                            <li><a class="dropdown-item" href="#">Улгуржи савдо фаолиятини лицензиялаш</a></li>
+                            <li><a class="dropdown-item" href="#">Давлат харидларини амалга ошириш</a></li>
+                            <li><a class="dropdown-item" href="#">Транспорт хизматларини ташкил этиш</a></li>
+                            <li><a class="dropdown-item" href="#">Автомобил ёнилғиси савдо фаолиятини лицензиялаш</a></li>
+                            <li><a class="dropdown-item" href="#">Ер муносабатларини тартибга солиш</a></li>
+                            <li><a class="dropdown-item" href="#">Очиқ маълумотлар</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Меъёрий ҳужжатлар</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Президент фармон, қарорлари</a></li>
+                            <li><a class="dropdown-item" href="#">Қонунчилик палатаси қарорлари</a></li>
+                            <li><a class="dropdown-item" href="#">Вазирлар Маҳкамаси қарорлари</a></li>
+                            <li><a class="dropdown-item" href="#">Туман ҳокими қарорлари</a></li>
+                            <li><a class="dropdown-item" href="#">Туман ҳокими фармойишлари</a></li>
+                            <li><a class="dropdown-item" href="#">Ўз кучини йўқотган меъёрий ҳужжатларon</a></li>
+                            <li><a class="dropdown-item" href="#">Туман ҳокимлиги иш юритиш йўриқномаси</a></li>
+                            <li><a class="dropdown-item" href="#">Расмий маърузалар</a></li>
+                            <li><a class="dropdown-item" href="#">Ишлаб чиқилган чора тадбирлар</a></li>
+                            <li><a class="dropdown-item" href="#">Аналитик хисобот ва тахлиллар</a></li>
+                            <li><a class="dropdown-item" href="#">Депутатлар фаолияти</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <main id="main">
+
+        <!-- ======= Breadcrumbs ======= -->
+        <section id="breadcrumbs" class="breadcrumbs">
+            <div class="container">
+
+                <ol>
+                    <li><a href="{{route('home')}}">Асосий сахифа</a></li>
+                    <li><a href="#">Маҳалла фуқоралар йиғинлари</a></li>
+                </ol>
+                <h2>Маҳалла фуқоралар йиғинлари</h2>
+
+            </div>
+        </section><!-- End Breadcrumbs -->
+
+        <!-- ======= Blog Section ======= -->
+        <section id="blog" class="blog">
+            <div class="container" data-aos="fade-up">
+
+                <div class="row">
+
+                    <div class="col-lg-8 entries">
+                    
+                        <div class="my-container">
+                            <div class="my-card">
+                                <img class="my-card-image">
+                                <div class="my-card-content">
+                                    <h3 id="title">Title</h3>
+                                    <p id="content">Content</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="my-container">
+                            <div class="my-card">
+                                <img class="my-card-image">
+                                <div class="my-card-content">
+                                    <h3 id="title">Title</h3>
+                                    <p id="content">Content</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="my-container">
+                            <div class="my-card">
+                                <img class="my-card-image">
+                                <div class="my-card-content">
+                                    <h3 id="title">Title</h3>
+                                    <p id="content">Content</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="my-container">
+                            <div class="my-card">
+                                <img class="my-card-image">
+                                <div class="my-card-content">
+                                    <h3 id="title">Title</h3>
+                                    <p id="content">Content</p>
+                                </div>
+                            </div>
+                        </div>
+                    
+                        <div class="my-container">
+                            <div class="my-card">
+                                <img class="my-card-image">
+                                <div class="my-card-content">
+                                    <h3 id="title">Title</h3>
+                                    <p id="content">Content</p>
+                                </div>
+                            </div>
+                        </div>
+                    
+                        <div class="my-container">
+                            <div class="my-card">
+                                <img class="my-card-image">
+                                <div class="my-card-content">
+                                    <h3 id="title">Title</h3>
+                                    <p id="content">Content</p>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    
+                    </div><!-- End blog entries list -->
+
+                    <div class="col-lg-4">
+
+                        <div class="sidebar">
+
+                            <h3 class="sidebar-title">Кидириш</h3>
+                            <div class="sidebar-item search-form">
+                                <form action="">
+                                    <input type="text">
+                                    <button type="submit"><i class="bi bi-search"></i></button>
+                                </form>
+                            </div><!-- End sidebar search formn-->
+                        </div><!-- End blog sidebar -->
+
+                    </div>
+
+                </div>
+        </section><!-- End Blog Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    
+    <footer-component></footer-component>
+
+    <!-- End Footer -->
+
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/commponent.js"></script>
+
+</body>
+
+</html>
